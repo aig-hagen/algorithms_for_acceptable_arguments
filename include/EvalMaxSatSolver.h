7298988c020b6dda8ca3ebdeb08b8a7baa7a4629
Lars <lars.bengel@fernuni-hagen.de>
2024-03-17 16:43:00 +0100

    added interface for EvalMaxSatSolver

---
+++ include/EvalMaxSatSolver.h
@@ -0,0 +1,23 @@
+#ifndef EVALMAX_SATSOLVER_H
+#define EVALMAX_SATSOLVER_H
+
+//#include "SATSolver.h"
+#include <EvalMaxSAT/lib/EvalMaxSAT/src/EvalMaxSAT.h>
+
+
+class EvalMaxSatSolver {
+
+private:
+	EvalMaxSAT<> solver;
+	int32_t decision_vars;
+
+public:
+	EvalMaxSatSolver(int32_t n_vars, int32_t n_args);
+	~EvalMaxSatSolver() {};
+	void add_hard_clause(const std::vector<int32_t> & clause);
+    void add_soft_clause(const std::vector<int32_t> & clause);
+	int solve();
+	std::vector<bool> model;
+
+};
+#endif
---
+++ src/EvalMaxSatSolver.cpp
@@ -0,0 +1,30 @@
+#include "EvalMaxSatSolver.h"
+
+
+EvalMaxSatSolver::EvalMaxSatSolver(int32_t n_vars, int32_t n_args) {
+	decision_vars = n_args;
+    for (uint32_t i = 0; i < n_vars; i++) {
+        solver.newVar();
+    }
+	model = std::vector<bool>(n_vars);
+}
+
+void EvalMaxSatSolver::add_hard_clause(const std::vector<int32_t> & clause) {
+	solver.addClause(clause);
+}
+
+void EvalMaxSatSolver::add_soft_clause(const std::vector<int32_t> & clause) {
+	solver.addClause(clause, 1);
+}
+
+int EvalMaxSatSolver::solve() {
+	bool sat = solver.solve();
+    if (!sat) {
+        return 20;
+    } else {
+        model.clear();
+		for (int32_t i = 0; i < decision_vars; i++)
+			model[i] = solver.getValue(i+1);
+	}
+	return sat;
+}
