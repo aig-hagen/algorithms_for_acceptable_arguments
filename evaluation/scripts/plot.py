8a99ab695506e532f5093f0a1875d84bcb36490d
Lars <lars.bengel@fernuni-hagen.de>
2025-03-27 13:36:41 +0100

    updated evaluation scripts

--- evaluation/scripts/plot.py
+++ evaluation/scripts/plot.py
@@ -15,66 +15,44 @@ def read_csv_to_dataframe(file_path):
         print("An error occurred:", e)
         sys.exit(1)
 
+def filter_dataframe(dataframe: pd.DataFrame, benchmark: str, problem: str):
+    df = dataframe[dataframe["task"] == problem]
+    df = df[df["benchmark_name"] == benchmark]
+    return df
+
+def sanitize_dataframe(dataframe: pd.DataFrame):
+    df = dataframe
+    return df
+
+
 if __name__ == "__main__":
     # Check if file path is provided as command-line argument
-    if len(sys.argv) != 3:
+    if len(sys.argv) != 4:
         print("Usage: python script.py <file_path> <output_file>")
         sys.exit(1)
     
     file_path = sys.argv[1]
-    problem = sys.argv[2] #"EC-ST"
+    benchmark = sys.argv[2] #"ICCMA17"
+    problem = sys.argv[3] #"EC-ST"
+
     timeout = 1200
-    output_file = problem.lower() + ".pgf"
+    enable_vbs = False
+    output_file = benchmark.lower() + "_" + problem.lower() + ".png"
+
     
     # Prepare DataFrame
     dataframe = read_csv_to_dataframe(file_path)
-    dataframe = dataframe[dataframe["task"] == problem]
-
-    print(dataframe)
-
-    dataframe = dataframe[dataframe["solver_name"] != "FUDGE-cadical"]
-
-    print(dataframe)
-
+    dataframe = filter_dataframe(dataframe, benchmark, problem)
     dataframe = dataframe.astype({'runtime': 'float'})
-
-    print(dataframe)
 
     dataframe = dataframe[dataframe["timed_out"] == "False"]
 
-    print(dataframe)
-
-    #dataframe = dataframe[dataframe["benchmark_name"] != "ICCMA15"]
-    #dataframe = dataframe[dataframe["benchmark_name"] != "ICCMA19"]
-    #dataframe = dataframe[dataframe["benchmark_name"] != "ICCMA17"]
-    dataframe = dataframe[dataframe["benchmark_name"] == "ICCMA23"]
-
-    print(dataframe)
-
-    # Define markers for each group
-    if problem.count("EC") > 0:
-        iaq = "IAQ$^c$"
-        eee = "EEE$^c$"
-        see = "SEE$^c$"
-        seem = "SEEM$^c$"
-    else:
-        iaq = "IAQ$^s$"
-        eee = "EEE$^s$"
-        see = "SEE$^s$"
-        seem = "SEEM$^s$"
-    
-    style_map = {
-        iaq: {'marker': 'x', 'linestyle': '--'},   # Circle, Solid line
-        eee: {'marker': 'o', 'linestyle': '-'},  # X, Dashed line
-        see: {'marker': '^', 'linestyle': ':'},  # Triangle, Dash-dot line
-        seem: {'marker': 'D', 'linestyle': '-.'}    # Diamond, Dotted line
-    }
-
-    #colors = {iaq: 'orange', eee: 'blue', see: 'green', seem: 'red'}  # Define colors
-
-    custom_order = ["IAQ-cgss2", "EEE-cgss2", "SEE-cgss2", "SEEM-cgss2"]
-    #custom_order = ["IAQ-cgss2", "SEE-cgss2"]
-    #custom_order = ["IAQ-cadical","EEE-cadical"]
+    style_map = [
+        {'marker': 'x', 'linestyle': '--'},
+        {'marker': 'o', 'linestyle': '-'},
+        {'marker': '^', 'linestyle': ':'},
+        {'marker': 'D', 'linestyle': '-.'}
+    ]
 
     # Group by solver
     grouped_dataframe = dataframe.groupby("solver_name")
@@ -89,35 +67,18 @@ if __name__ == "__main__":
     # Create figure and axis objects
     fig, ax = plt.subplots()
 
-    for slv in custom_order:
-        name = slv
-        group = grouped_dataframe.get_group(slv)
-    # Print the grouped DataFrame
-    #for i, (name, group) in enumerate(grouped_dataframe):
-        if name != "VBS":
-            name = name.split("-")[0]
-            if problem.count("EC") > 0:
-                name += "$^c$"
-            else:
-                name += "$^s$"
-        #print(name)
-        print(len(group["instance"].tolist()))
-        #print(group[group["runtime"] < 5]["benchmark_name"].value_counts())
-        #group = group[group["instance"].isin(problem_instances)]
+    for i, (name, group) in enumerate(grouped_dataframe):
         group = group.sort_values(by="runtime")
         group = group.reset_index(drop=True)
-        ax.plot(group["runtime"], group.index, marker=style_map[name]['marker'], markersize=2, markerfacecolor='none', linewidth=1, linestyle=style_map[name]['linestyle'], label=name)
+        ax.plot(group["runtime"], group.index, marker=style_map[i]['marker'], markersize=2, markerfacecolor='none', linewidth=1, linestyle=style_map[i]['linestyle'], label=name)
 
     ax.axvline(x=timeout, color='r', linestyle='--', label=None)
 
     # Add labels and title
     ax.set_ylabel("instances solved")
     ax.set_xlabel("runtime (s)")
-    #ax.set_title("Line Plot of Runtime for Each Group")
     ax.legend(loc='lower right', fontsize=15, markerscale=2.5, handlelength=2, handletextpad=1)
-
     ax.set_ylim(0, None)
-    #ax.set_xscale('log')
     
     # Save plot to file
     if output_file.endswith(".pgf"):
@@ -126,39 +87,3 @@ if __name__ == "__main__":
         plt.savefig(output_file, format="png")
     else:
         raise NameError("Unsupported Output type")
-    
-
-"""
-    df_results = pd.DataFrame(columns=["Dataset", "#Instances", "AVG ARGS", "MED ARGS", "STD ARGS", "MIN ARGS", "MAX ARGS", "AVG ATTS", "MED ATTS", "STD ATTS", "MIN ATTS", "MAX ATTS", "AVG DEG"])
-
-    df = pd.read_csv("statistics/ICCMA21.csv")
-
-    row = {"Dataset": "ICCMA21", "#Instances": len(df["Instance"].unique()),
-        "AVG ARGS": df["#Args"].mean(),"MED ARGS": df["#Args"].median(), 
-        "STD ARGS": df["#Args"].std(),
-        "MIN ARGS": df["#Args"].min(),"MAX ARGS": df["#Args"].max(), 
-        "AVG ATTS": df["#Attacks"].mean(), "MED ATTS": df["#Attacks"].median(),
-        "STD ATTS": df["#Attacks"].std(),
-        "MIN ATTS": df["#Attacks"].min(), "MAX ATTS": df["#Attacks"].max(),
-        "AVG DEG": df["AVG DEG"].mean(),
-        }
-    df_results = df_results._append(row, ignore_index=True)
-
-    print(problem_instances)
-    df = df[df["Instance"].isin(problem_instances)]
-
-    row = {"Dataset": "ICCMA21", "#Instances": len(df["Instance"].unique()),
-        "AVG ARGS": df["#Args"].mean(),"MED ARGS": df["#Args"].median(), 
-        "STD ARGS": df["#Args"].std(),
-        "MIN ARGS": df["#Args"].min(),"MAX ARGS": df["#Args"].max(), 
-        "AVG ATTS": df["#Attacks"].mean(), "MED ATTS": df["#Attacks"].median(),
-        "STD ATTS": df["#Attacks"].std(),
-        "MIN ATTS": df["#Attacks"].min(), "MAX ATTS": df["#Attacks"].max(),
-        "AVG DEG": df["AVG DEG"].mean(),
-        }
-    df_results = df_results._append(row, ignore_index=True)
-
-
-    print(df_results)
-
-    df_results.to_latex('table.tex', index=False, float_format="%.0f")"""
--- evaluation/scripts/table.py
+++ evaluation/scripts/table.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import sys
 
-def read_csv_to_dataframe(file_path):
+def read_csv_to_dataframe(file_path: str):
     try:
         # Read CSV into a pandas DataFrame
         df = pd.read_csv(file_path)
@@ -15,6 +15,25 @@ def read_csv_to_dataframe(file_path):
         print("An error occurred:", e)
         sys.exit(1)
 
+def filter_dataframe(dataframe: pd.DataFrame, benchmark: str, problem: str):
+    df = dataframe[dataframe["task"] == problem]
+    df = df[df["benchmark_name"] == benchmark]
+    return df
+
+def restructure_dataframe(dataframe: pd.DataFrame):
+    solvers = dataframe["solver_name"].unique().tolist()
+    df = pd.DataFrame()
+    for slv in solvers:
+        view = dataframe[dataframe["solver_name"] == slv]
+        df[slv] = view["runtime"].tolist()
+    return df
+
+def compute_vbs(dataframe: pd.DataFrame):
+    solvers = dataframe.columns.tolist()
+    dataframe['contributor'] = dataframe.idxmin(axis=1)
+    dataframe['VBS'] = dataframe[solvers].min(axis=1)
+    return dataframe
+
 if __name__ == "__main__":
     # Check if file path is provided as command-line argument
     if len(sys.argv) != 4:
@@ -24,90 +43,63 @@ if __name__ == "__main__":
     file_path = sys.argv[1]
     benchmark = sys.argv[2] #"ICCMA19"
     problem = sys.argv[3] #"EC-ST"
+    
     timeout = 1200
-    output_file = benchmark.lower() + "_" + problem.lower() + ".pgf"
+    enable_vbs = False
+    output_file = benchmark.lower() + "_" + problem.lower() + ".tex"
     
     # Prepare DataFrame
     dataframe = read_csv_to_dataframe(file_path)
-    dataframe = dataframe[dataframe["task"] == problem]
-    dataframe = dataframe[dataframe["benchmark_name"] == benchmark]
-
-    dataframe = dataframe[dataframe["solver_name"] != "FUDGE-cadical"]
-
+    dataframe = filter_dataframe(dataframe, benchmark, problem)
     dataframe = dataframe.astype({'runtime': 'float'})
 
-    # Group by 'group' and extract the 'runtime' for each group
-    solvers = dataframe["solver_name"].unique().tolist()
-    #solvers = ["IAQ-cgss2", "EEE-cgss2", "SEE-cgss2", "SEEM-cgss2"]
-    new_df = pd.DataFrame()
-    for slv in solvers:
-        partial = dataframe[dataframe["solver_name"] == slv]
-        print(partial["runtime"])
-        new_df[slv] = partial["runtime"].tolist()
+    #dataframe = dataframe[dataframe["solver_name"] != "FUDGE-cadical"]
 
-    #new_df['contributor'] = new_df.idxmin(axis=1)
-    #new_df['VBS'] = new_df[["IAQ-cgss2", "EEE-cgss2", "SEE-cgss2", "SEEM-cgss2"]].min(axis=1)
-    #new_df['VBS'] = new_df[["IAQ-cadical", "EEE-cadical"]].min(axis=1)
+    dataframe = restructure_dataframe(dataframe)
 
+    if enable_vbs:
+        dataframe = compute_vbs(dataframe)
 
     table_data = []
 
-    #solvers.append("VBS")
+    solvers = dataframe.columns.tolist()
+    if enable_vbs:
+        solvers.remove("contributor")
 
     for name in solvers:
-        num_rows = len(new_df[name])
-        timeouts = new_df[name].eq(timeout).sum()
-        total_runtime = new_df[name].sum() - (timeouts* timeout)
-        par_10 = (new_df[name].sum() + (9 * timeouts * timeout)) / num_rows
-        if name != "VBS":
-            vbs="-"#vbs = new_df["contributor"].value_counts().get(name, 0)
-            #name = "\\" + name.split("-")[0]
-            if problem.count("EC") > 0:
-                name += "$^c$"
+        num_rows = len(dataframe[name])
+        timeouts = dataframe[name].eq(timeout).sum()
+        total_runtime = dataframe[name].sum() - (timeouts* timeout)
+        par_10 = (dataframe[name].sum() + (9 * timeouts * timeout)) / num_rows
+        if enable_vbs:
+            if name != "VBS":
+                vbs = dataframe["contributor"].value_counts().get(name, 0)
             else:
-                name += "$^s$"
-        else:
-            vbs = "-"
+                vbs = "-"
+        #if name != "VBS":
+        #    vbs="-"#vbs = new_df["contributor"].value_counts().get(name, 0)
+        #    name = "\\" + name.split("-")[0]
+        #    if problem.count("EC") > 0:
+        #        name += "$^c$"
+        #    else:
+        #        name += "$^s$"
+        #else:
+        #    vbs = "-"
 
-        table_data.append([name, num_rows, timeouts, total_runtime, par_10, vbs])
-    table_df = pd.DataFrame(table_data, columns=["Algorithm", "N", "#TO", "RT", "PAR10", "#VBS"])
+        if enable_vbs:
+            table_data.append([name, num_rows, timeouts, total_runtime, par_10, vbs])
+        else:
+            table_data.append([name, num_rows, timeouts, total_runtime, par_10])
+
+    if enable_vbs:
+        table_df = pd.DataFrame(table_data, columns=["Algorithm", "N", "#TO", "RT", "PAR10", "#VBS"])
+    else:
+        table_df = pd.DataFrame(table_data, columns=["Algorithm", "N", "#TO", "RT", "PAR10"])
+
     table_df.sort_values(["#TO", "RT"], inplace=True)
     table_df.index = np.arange(1, len(table_df)+1)
     table_df.index.name = "No."
     table_df.reset_index(inplace=True)
-
-    """
-    # Group by solver
-    grouped_dataframe = dataframe.groupby("solver_name")
-    
-    # Create a table with one row for each group
-    table_data = []
-    for name, group in grouped_dataframe:
-        num_rows = len(group)
-        timeouts = group["runtime"].eq(timeout).sum()
-        total_runtime = group["runtime"].sum() - (timeouts* timeout)
-        par_10 = (group['runtime'].sum() + (9 * timeouts * timeout)) / num_rows
-
-        if name != "VBS":
-            view = dataframe[dataframe["solver_name"] == "VBS"]
-            vbs = view["contributor"].value_counts().get(name, 0)
-            name = "\\" + name.split("-")[0]
-            if problem.count("EC") > 0:
-                name += "$^c$"
-            else:
-                name += "$^s$"
-        else:
-            vbs = "-"
-
-        
-            
-        table_data.append([name, num_rows, timeouts, total_runtime, par_10, vbs])
-    table_df = pd.DataFrame(table_data, columns=["Algorithm", "N", "#TO", "RT", "PAR10", "#VBS"])
-    table_df.sort_values(["#TO", "RT"], inplace=True)
-    table_df.index = np.arange(1, len(table_df)+1)
-    table_df.index.name = "No."
-    table_df.reset_index(inplace=True)
-    """
     
     # Save table to file
-    table_df.to_latex(output_file.replace('.pgf', '_table.tex').replace('.png', '_table.tex'), index=False, float_format="%.2f")
+    table_df.to_latex(output_file, index=False, float_format="%.2f")
